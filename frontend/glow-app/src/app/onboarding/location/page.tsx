4d0cd28a00f3c703c7a17d833932e912ba12374c
Danny Garcia <dgarcia1724@berkeley.edu>
2025-05-14 18:20:14 -0700

    location page.tsx started

---
+++ frontend/glow-app/src/app/onboarding/location/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Location() {
+  return (
+    <div className="min-h-screen flex flex-col bg-white justify-center items-center">
+      <h1 className="text-3xl sm:text-4xl font-extrabold text-black text-center">
+        Where do u live
+      </h1>
+    </div>
+  );
+}
