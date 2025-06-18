470756ea9edcc3d954739f560aec6c0726a54bcc
Danny Garcia <dgarcia1724@berkeley.edu>
2025-06-17 22:25:40 -0700

    added tooltip for non-negotiable

--- frontend/glow-app/src/app/main-app/filters/filter-options/religion/page.tsx
+++ frontend/glow-app/src/app/main-app/filters/filter-options/religion/page.tsx
@@ -101,7 +101,26 @@ export default function Religion() {
                     onChange={handleNonNegotiableChange}
                     className="form-checkbox h-5 w-5 text-yellow-400 rounded border-gray-300 focus:ring-yellow-400 cursor-pointer"
                   />
-                  <span className="text-gray-700">Non-negotiable for me</span>
+                  <span className="text-gray-700">Non-negotiable</span>
+                  <div className="relative group">
+                    <svg
+                      className="w-4 h-4 text-gray-400 cursor-help"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 text-sm text-white bg-gray-900 rounded-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200 whitespace-nowrap z-10">
+                      Your profile will only be visible to people who meet this
+                      preference.
+                      <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-900"></div>
+                    </div>
+                  </div>
                 </label>
               </div>
             )}
